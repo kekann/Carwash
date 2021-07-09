1e289dde431ec9ef9a3ac6a841de6959a8ef38da
Jan Zenger <janek.zeng@gmail.com>
2021-07-09 18:03:04 +0200

    Add project files.

---
+++ MyjniaSamochodowa/Input.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyjniaSamochodowa
+{
+    class Input
+    {
+        public static int InputCarwash()
+        {
+            Console.WriteLine("Give me a number of Carwashes in this simulation, please:");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+        public static int InputCars()
+        {
+            Console.WriteLine("Give me a number of Cars in this simulation, please:");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
---
+++ MyjniaSamochodowa/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyjniaSamochodowa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NumberOfCarwashes = Input.InputCarwash();
+            int NumberOfCars = Input.InputCars();
+        }
+    }
+}
